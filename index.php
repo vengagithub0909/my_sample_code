1b5f0669c104031a77cedea0f55aec2d5ae0d52c
mahipalk <mahipalk@menlo-technologies.com>
2018-06-07 14:30:28 +0530

    File updated second time

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 $my_file = date('ymj-ms').'_file.txt';
 $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-$data = 'This is the data';
+$data = 'This is the data samplee file';
 fwrite($handle, $_REQUEST);
 ?>
